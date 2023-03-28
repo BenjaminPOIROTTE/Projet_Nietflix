d14ad0b0d0d586c8cd0921d970861cddb5169758
Cliffy57 <73029436+Cliffy57@users.noreply.github.com>
2023-03-28 15:59:32 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <sessions-vue></sessions-vue>
+  </div>
+</template>
+
+<script>
+import SessionsVue from './components/SessionsVue.vue';
+
+export default {
+  components: {
+    SessionsVue
+  },
+  // other Vue component options
+}
+</script>
