1a5f6512a350a567a40cca832d15918b7c88587d
TakyL <takinou57@gmail.com>
2023-03-28 17:15:11 +0200

    clears somes comments

--- Rest/Back/application/app.js
+++ Rest/Back/application/app.js
@@ -1,13 +1,3 @@
-/*const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://achlys:<No0OT1v5vcURFfEY>@cinemarestapi.qx37mhn.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("sample_mflix").collection("comments");
-  // perform actions on the collection object
-  console.log("Une erreur est survenue \n"+err);
-
-  client.close();
-});*/
 const path = require('path');
 const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
 const express = require("express");
@@ -95,53 +85,3 @@ app.delete("/sessions/:id", (req, res) => {
     }
   });
 });
-
-/*
-// Get a specific film session
-app.get('/sessions/:id', (req, res) => {
-  // Use the MongoDB driver to get a specific film session by id
-  collection.findOne({ _id: req.params.id }, (err, session) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(session);
-    }
-  });
-});
-
-// Add a new film session
-app.post('/sessions', (req, res) => {
-  // Use the MongoDB driver to insert a new film session into the database
-  collection.insertOne(req.body, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(result.ops[0]);
-    }
-  });
-});
-
-// Update an existing film session
-app.put('/sessions/:id', (req, res) => {
-  // Use the MongoDB driver to update an existing film session
-  collection.updateOne({ _id: req.params.id }, { $set: req.body }, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-// Delete a film session
-app.delete('/sessions/:id', (req, res) => {
-  // Use the MongoDB driver to delete a film session
-  collection.deleteOne({ _id: req.params.id }, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-*/
